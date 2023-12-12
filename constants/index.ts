f893513e9cd7dc6ebb8a99f023ceb639ab587bb7
Modou Tamba <tambamodou68@gmail.com>
2023-12-12 12:39:11 -0800

    Travel-App-UI with NextJs

---
+++ constants/index.ts
@@ -0,0 +1,86 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: '/', key: 'home', label: 'Home' },
+  { href: '/', key: 'how_traveller_work', label: 'How Traveller Work?' },
+  { href: '/', key: 'services', label: 'Services' },
+  { href: '/', key: 'pricing ', label: 'Pricing ' },
+  { href: '/', key: 'contact_us', label: 'Contact Us' },
+];
+
+// CAMP SECTION
+export const PEOPLE_URL = [
+  '/person-1.png',
+  '/person-2.png',
+  '/person-3.png',
+  '/person-4.png',
+];
+
+// FEATURES SECTION
+export const FEATURES = [
+  {
+    title: 'Real maps can be offline',
+    icon: '/map.svg',
+    variant: 'blue',
+    description:
+      'We provide a solution for you to be able to use our application when climbing, yes offline maps you can use at any time there is no signal at the location',
+  },
+  {
+    title: 'Set an adventure schedule',
+    icon: '/calendar.svg',
+    variant: 'blue',
+    description:
+      "Schedule an adventure with friends. On holidays, there are many interesting offers from Traveller. That way, there's no more discussion",
+  },
+  {
+    title: 'Technology using augment reality',
+    icon: '/tech.svg',
+    variant: 'blue',
+    description:
+      'Technology uses augmented reality as a guide to your traveller trail in the forest to the top of the mountain. Already supported by the latest technology without an internet connection',
+  },
+  {
+    title: 'Many new locations every month',
+    icon: '/location.svg',
+    variant: 'orange',
+    description:
+      'Lots of new locations every month, because we have a worldwide community of climbers who share their best experiences with climbing',
+  },
+];
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    title: 'Learn More',
+    links: [
+      'About Traveller',
+      'Press Releases',
+      'Environment',
+      'Jobs',
+      'Privacy Policy',
+      'Contact Us',
+    ],
+  },
+  {
+    title: 'Our Community',
+    links: ['Climbing xixixi', 'Traveller traveller', 'traveller camp'],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: 'Contact Us',
+  links: [
+    { label: 'Admin Officer', value: '+2203334242' },
+    { label: 'Email Officer', value: 'traveller@gmail.com' },
+  ],
+};
+
+export const SOCIALS = {
+  title: 'Social',
+  links: [
+    '/facebook.svg',
+    '/instagram.svg',
+    '/twitter.svg',
+    '/youtube.svg',
+    '/wordpress.svg',
+  ],
+};
